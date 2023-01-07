e5fbd2e5afa8bd395543beadca022185f515b016
Nyamdorj Elbeg <nyamaa@Nyamdorj.local>
2023-01-08 02:59:07 +0800

    cors

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py runscript load_lottery
-uwsgi --socket :8000 --master --enable-threads --module mysite.wsgi
+uwsgi --socket :8080 --master --enable-threads --module mysite.wsgi
